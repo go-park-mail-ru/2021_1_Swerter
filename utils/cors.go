f24f45e6653d8b6f00ffc6d18aecb26348a8d22b
Ilia Muravev <degtyarev327@gmail.com>
2021-03-10 12:04:11 +0300

    fix cookies

--- utils/cors.go
+++ utils/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func SetupCORS(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "https://my-motivation.netlify.app")
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
-	(*w).Header().Set("Same-Site", "None")
+	(*w).Header().Set("SameSite", "None")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
